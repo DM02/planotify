71a0ba78b7c5394be5e97a0cb10cb46eae330b6f
Dominik <mordi0917@gmail.com>
2022-03-06 16:50:37 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/DM02/stream-recorder
+
+go 1.17
+
+require github.com/PuerkitoBio/goquery v1.8.0
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/ecnepsnai/discord v1.2.1
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/ecnepsnai/discord"
+)
+
+var globalElement string = ""
+
+func main() {
+	discord.WebhookURL = "https://discord.com/api/webhooks/949441185364209754/u_iKxGHfRH90Vvqd8wt0J--i7FPBoqnezaHRf3T9Akb0hM05DupD7UKeB5I__ElCRE5-"
+	// Request the HTML page.
+	res, err := http.Get("http://zsz1.edu.pl/plan/plany/o26.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	element := doc.Find("body > div > table > tbody > tr:nth-child(2) > td.op > table > tbody > tr > td:nth-child(1)")
+	globalElement = element.Text()
+	for {
+		//http://localhost/Plan%20lekcji%20oddzia%c5%82u%20-%203pTP.html
+		//http://zsz1.edu.pl/plan/plany/o26.html
+		res, err := http.Get("http://zsz1.edu.pl/plan/plany/o26.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if res.StatusCode != 200 {
+			log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		}
+
+		// Load the HTML document
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Find the review items
+		element := doc.Find("body > div > table > tbody > tr:nth-child(2) > td.op > table > tbody > tr > td:nth-child(1)")
+
+		if globalElement == element.Text() {
+
+			fmt.Println("Brak zmian", time.Now())
+
+		} else {
+			globalElement = element.Text()
+
+			fmt.Println("Nastapila zmiana ", globalElement)
+			fmt.Println("O godzinie: ", time.Now())
+			discord.Say(fmt.Sprintf("Nastapila zmiana planu w dniu: %s", time.Now().Format("Monday, 02-Jan-06 15:04:05 MST")))
+			discord.Say("http://zsz1.edu.pl/plan/plany/o26.html")
+
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+}
